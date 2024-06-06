359a6fafb3f71f2075571254db984e65279dc3ad
CODSensei <krishna2342005@gmail.com>
2024-06-06 08:14:32 +0530

    complete-setup-with-tailwindcss-and-onboarding-pages

---
+++ App.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Onboarding1 from './components/Onboarding1';
+import Onboarding2 from './components/Onboarding2';
+import Onboarding3 from './components/Onboarding3';
+import Questions from './components/Questions';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding1" component={Onboarding1} />
+        <Stack.Screen name="Onboarding2" component={Onboarding2} />
+        <Stack.Screen name="Onboarding3" component={Onboarding3} />
+        <Stack.Screen name="Questions" component={Questions} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ components/Onboarding3.js
@@ -0,0 +1,47 @@
+import {
+    ImageBackground,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+  import React from 'react';
+  
+  const Onboarding3 = ({navigation}) => {
+    return (
+      <View className="flex flex-1 justify-center">
+        <ImageBackground
+          source={require('../assets/Welcome3.png')}
+          resizeMode="cover"
+          className="h-screen w-screen flex flex-1 flex-col">
+          <View
+            className="h-80 w-screen flex flex-col
+        justify-end absolute bottom-0 rounded-t-3xl">
+           
+            <View className="flex flex-row justify-between">
+              <TouchableOpacity
+                className="mx-8 my-8 text-slate-300"
+                onPress={() => {
+                  navigation.navigate('Login');
+                }}>
+                <Text className="text-white text-lg">SKIP</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="mx-8 my-8"
+                onPress={() => {
+                  navigation.navigate('Login');
+                }}>
+                <Text className="text-white text-lg">NEXT</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  };
+  
+  export default Onboarding3;
+  
+  const styles = StyleSheet.create({});
+  
